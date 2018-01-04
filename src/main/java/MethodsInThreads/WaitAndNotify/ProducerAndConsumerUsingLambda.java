15ebf70c6797ea0e008dbbe4bd89977439b1f8a1
arun789 <adwiti1975@gmail.com>
2018-01-04 00:08:05 -0700

    Re-entrant Lock

---
+++ src/main/java/MethodsInThreads/WaitAndNotify/ProducerAndConsumerUsingLambda.java
@@ -0,0 +1,4 @@
+package MethodsInThreads.WaitAndNotify;
+
+public class ProducerAndConsumerUsingLambda {
+}
---
+++ src/main/java/ReEntrantsLockExample/ReEntrantsLockExplained.java
@@ -0,0 +1,36 @@
+package ReEntrantsLockExample;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReEntrantsLockExplained {
+    private static int counter;
+    private static Lock lock = new ReentrantLock();
+
+    private static int increment() {
+        lock.lock();
+        try {
+            for (int i = 0; i < 1000; i++) {
+                counter++;
+            }
+        } finally {
+            lock.unlock();
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r = () -> increment();
+
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("counter " + counter);
+    }
+}
