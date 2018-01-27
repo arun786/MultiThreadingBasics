8953a06d866f2954fa9f50ba3271851822d4c16d
Adwiti789 <aruntcs2005@gmail.com>
2018-01-27 00:35:48 -0700

    Semaphore

--- src/main/java/SempahoreExplained/SemaphoreExplained.java
+++ src/main/java/SempahoreExplained/SemaphoreExplained.java
@@ -1,4 +1,52 @@
 package SempahoreExplained;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 public class SemaphoreExplained {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 12; i++) {
+            executorService.submit(() -> {
+                DownLoader.INSTANCE.downloadData();
+            });
+        }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+enum DownLoader {
+
+    INSTANCE;
+    private Semaphore semaphore = new Semaphore(3, true);
+
+    public void downloadData() {
+        try {
+            semaphore.acquire();
+            download();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Finished downloading...");
+            semaphore.release();
+        }
+    }
+
+    private void download() {
+        System.out.println("Downloading data.....");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
